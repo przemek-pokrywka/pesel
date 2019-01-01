89d8e016f52cd5e7769c5cfbd68c6bfbc09cff6c
Mateusz Czeladka <mateusz.szczap@gmail.com>
2019-01-01 20:23:51 +0100

    Modernizacja

---
+++ build.sbt
@@ -0,0 +1,9 @@
+organization := "gtug.wroclaw"
+
+name := "pesel"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.specs2" %% "specs2-core" % "3.8.9" % Test
--- src/test/scala/gtug/wroclaw/pesel/PeselSpec.scala
+++ src/test/scala/gtug/wroclaw/pesel/PeselSpec.scala
@@ -5,17 +5,18 @@ import org.specs2.mutable._
 class PeselSpec extends Specification {
 
   "The PESEL helper" should {
-    
+
     "recognize valid PESEL" in {
       Pesel.valid("78040105890") must beTrue
     }
-    
+
     "treat too long PESELs as invalid" in {
       Pesel.valid("0000000000000000") must beFalse
     }
-    
+
     "treat PESELs with non-existent dates as invalid" in {
       Pesel.valid("01047805890") must beFalse
     }
   }
+
 }
